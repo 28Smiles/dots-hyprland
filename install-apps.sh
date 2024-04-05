0eccd8bc3f13cd3e009da588f1249173d3432ebe
Leon Camus <leon.c@gmx.de>
2024-04-05 15:13:48 +0200

    transparent term

--- install-apps.sh
+++ install-apps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo pacman -Suy firefox code docker docker-compose discord
+sudo pacman -Suy firefox code docker docker-compose
 sudo systemctl enable docker.socket
 sudo systemctl start docker.socket
-yay -Suy thorium-browser intellij-idea-ultimate-edition spotifyd spotify-qt lazydocker
+yay -Suy thorium-browser intellij-idea-ultimate-edition spotifyd spotify-qt lazydocker discord_arch_electron
