1677d052c33ed803ebfbd3e09df1f686b4b13f06
Leon Camus <leon.c@gmx.de>
2024-04-07 23:02:24 +0200

    sddm wayland

--- install-sddm-sugar-dark.sh
+++ install-sddm-sugar-dark.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 INSTALL_PATH=/usr/share/sddm/themes/sugar-dark
+sudo pacman -Suy weston
 sudo git clone https://github.com/28Smiles/sddm-sugar-dark.git $INSTALL_PATH
 [ getent group sddm-themes ] || sudo groupadd sddm-themes
 v sudo usermod -aG sddm-themes "$(whoami)"
@@ -9,5 +10,8 @@ sudo chown :sddm-themes $INSTALL_PATH/theme.conf
 sudo chmod 664 $INSTALL_PATH/theme.conf
 
 sudo mkdir /etc/sddm.conf.d
-sudo bash -c "echo '[Theme]
+sudo bash -c "echo '[General]
+DisplayServer=wayland
+
+[Theme]
 Current=sugar-dark' > /etc/sddm.conf.d/sugar-dark.conf"
