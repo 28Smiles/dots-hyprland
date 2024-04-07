7b50be66ea9f9bcb0b8300d5c1c4f1d36ad38afe
Leon Camus <leon.c@gmx.de>
2024-04-07 17:33:18 +0200

    feat: Sddm theme

--- .config/ags/scripts/color_generation/switchwall.sh
+++ .config/ags/scripts/color_generation/switchwall.sh
@@ -25,17 +25,20 @@ else
     --transition-pos "$cursorposx, $cursorposy_inverted"
 fi
 
-# Set sddm wallpaper
-if [ ! -f "/usr/share/sddm/themes/sugar-dark/Background.jpg" ]; then
-    echo "SDDM theme not found. Skipping wallpaper change."
-else
-    # Only jpg is supported by sddm
-    if [ "${imgpath: -4}" != ".jpg" ]; then
-        cat "$imgpath" > "/usr/share/sddm/themes/sugar-dark/Background.jpg"
+apply_sddm_sugar() {
+    if [ ! -f "/usr/share/sddm/themes/sugar-dark/Background.jpg" ]; then
+        echo "SDDM theme not found. Skipping wallpaper change."
     else
-        echo "Cannot set wallpaper for SDDM. Only jpg is supported."
+        # Only jpg is supported by sddm
+        if [ "${imgpath: -4}" != ".jpg" ]; then
+            cat "$imgpath" > "/usr/share/sddm/themes/sugar-dark/Background.jpg"
+        else
+            echo "Cannot set wallpaper for SDDM. Only jpg is supported."
+        fi
     fi
-fi
+}
+
+apply_sddm_sugar &
 
 # Generate colors for ags n stuff
 "$HOME"/.config/ags/scripts/color_generation/colorgen.sh "${imgpath}" --apply --smart
