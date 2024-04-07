135e19eb7878dcbbdc247319328274061f90373b
Leon Camus <leon.c@gmx.de>
2024-04-07 17:19:10 +0200

    sddm-theme-installer

--- install-sddm-sugar-dark.sh
+++ install-sddm-sugar-dark.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 INSTALL_PATH=/usr/share/sddm/themes/sugar-dark
 sudo git clone https://github.com/28Smiles/sddm-sugar-dark.git $INSTALL_PATH
-sudo chmod 775 $INSTALL_PATH/Background.jpg
-sudo chmod 775 $INSTALL_PATH/theme.conf
+sudo chmod ugo+rw $INSTALL_PATH/Background.jpg
+sudo chmod ugo+rw $INSTALL_PATH/theme.conf
 sudo mkdir /etc/sddm.conf.d
-sudo bash -c "echo '[Theme]\nCurrent=sugar-dark' > /etc/sddm.conf.d/00.conf"
+sudo bash -c "echo '[Theme]
+Current=sugar-dark' > /etc/sddm.conf.d/sugar-dark.conf"
